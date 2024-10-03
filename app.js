88bd2dbde47b330be4a3cd862837059dcfcc0d25
mamunarr6 <mamunarr6@gmail.com>
2024-10-03 13:23:13 +0600

    total cost done

--- app.js
+++ app.js
@@ -4,6 +4,27 @@ let count = 0;
 for (const btn of allCartBtn) {
     btn.addEventListener('click', (e) => {
         count += 1;
+        const placeName = e.target.parentNode.parentNode.childNodes[1].innerText;
+        const price = e.target.parentNode.parentNode.childNodes[3].childNodes[1].innerText;
+
+        const myTours = document.getElementById('my-tours');
+        const myTour = document.createElement('div');
+        const tours = document.createElement('p');
+        const hr = document.createElement('hr');
+
+        myTour.classList.add('my-tour');
+        tours.innerHTML = `${placeName} $ <span>${price}`;
+        hr.classList.add('border-red-200')
+
+        myTours.appendChild(myTour);
+        myTour.appendChild(tours);
+        myTour.appendChild(hr);
+
+        const totalCost = document.getElementById('total-cost').innerText;
+        const convertedTotalCost = parseInt(totalCost);
+        const sum = convertedTotalCost + parseInt(price)
+
+        setInnerText('total-cost', sum);
         setInnerText('cart-count', count);
     })
 }
